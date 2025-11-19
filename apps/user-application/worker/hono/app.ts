1fedf6f91c74f2ff752b1b18684ba8f756410740
zxzAli <blackbearddevss@gmail.com>
2025-11-19 13:32:32 -0500

    fix

--- apps/user-application/worker/hono/app.ts
+++ apps/user-application/worker/hono/app.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter } from "@/worker/trpc/router";
 import { createContext } from "@/worker/trpc/context";
+import {getAuth} from '@repo/data-ops/auth'
 
 export const App = new Hono<{ Bindings: ServiceBindings }>();
 
@@ -15,9 +16,18 @@ App.all("/trpc/*", (c) => {
   });
 }); // tRPC handler//  this is a where the frontend will call to connect to the backend data service for the click socket
 
+
 App.get("/click-socket", async (c) => {
   const headers = new Headers(c.req.raw.headers);
   headers.set("account-id", "1234567890");
   const proxiedRequest = new Request(c.req.raw, { headers });
   return c.env.BACKEND_SERVICE.fetch(proxiedRequest);
 });
+
+App.on(["POST","GET"],'/api/auth/*', (c) => {
+  const auth  = getAuth({
+   clientId: c.env.GOOGLE_CLIENT_ID,
+    clientSecret: c.env.GOOGLE_CLIENT_SECRET,
+  });
+  return auth.handler(c.req.raw);
+})
